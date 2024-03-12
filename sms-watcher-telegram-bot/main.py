e78d76381de65ae208e48e4f036b0889d3250a45
agorelik <Aleksandr_Gorelik@center.rt.ru>
2024-03-13 01:58:48 +0300

    Initial commit

--- sms-watcher-telegram-bot/main.py
+++ sms-watcher-telegram-bot/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import json
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import CommandStart
 
@@ -12,45 +13,37 @@ bot = Bot(token=TELEGRAM_TOKEN_API)
 dp = Dispatcher()
 access_err_msg = "Я могу отвечать только пользователям из белого списка"
 
+with open('config/config.json', 'r') as file:
+    data = json.load(file)
+
+sim_cards = data['sim-cards']
+
 
 @dp.message(CommandStart())
 async def cmd_start(message: types.Message) -> None:
     if message.from_user.id in USER_WHITELIST:
-        kb = [
-            [types.KeyboardButton(text="1")],
-            [types.KeyboardButton(text="2")],
-            [types.KeyboardButton(text="3")],
-        ]
+        kb = []
+        for sim_card in sim_cards:
+            kb.append([types.KeyboardButton(text=f"Phone: {sim_card['phone']}")])
         keyboard = types.ReplyKeyboardMarkup(
-            keyboard=kb,
-            resize_keyboard=True,
-            input_field_placeholder="Выберите номер"
+            keyboard=kb, resize_keyboard=True, input_field_placeholder="Выберите номер"
         )
         await message.answer("Выберите номер", reply_markup=keyboard)
     else:
         await message.answer(access_err_msg)
 
 
-@dp.message(F.text.lower() == "1")
-async def handle_number_1(message: types.Message):
+@dp.message(F.text.startswith("Phone: "))
+async def handle_phone(message: types.Message):
     if message.from_user.id in USER_WHITELIST:
-        await message.answer("Выбран номер 1")
-    else:
-        await message.answer(access_err_msg)
-
-
-@dp.message(F.text.lower() == "2")
-async def handle_number_2(message: types.Message):
-    if message.from_user.id in USER_WHITELIST:
-        await message.answer("Выбран номер 2")
-    else:
-        await message.answer(access_err_msg)
-
-
-@dp.message(F.text.lower() == "3")
-async def handle_number_3(message: types.Message):
-    if message.from_user.id in USER_WHITELIST:
-        await message.answer("Выбран номер 3")
+        target_phone = message.text[7:]
+        for sim_card in sim_cards:
+            if sim_card['phone'] == target_phone:
+                device_ip = sim_card['device_ip']
+                await message.answer(device_ip)
+                break
+        else:
+            await message.answer("Ошибка")
     else:
         await message.answer(access_err_msg)
 
