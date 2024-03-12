94169a493dadef9c0a1d10d3c42478cd69455a5a
agorelik <Aleksandr_Gorelik@center.rt.ru>
2024-03-13 00:43:47 +0300

    Initial commit

--- sms-watcher-telegram-bot/main.py
+++ sms-watcher-telegram-bot/main.py
@@ -3,41 +3,56 @@ import os
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import CommandStart
 
+TELEGRAM_TOKEN_API = os.environ.get("TELEGRAM_TOKEN_API", default="")
+users_str = os.environ.get("TELEGRAM_USER_WHITELIST")
+users = users_str.split(',')
+USER_WHITELIST = [int(item) for item in users if item.strip().isdigit()]
 
-TOKEN_API = os.environ.get("TOKEN_API", default="")
-
-bot = Bot(token=TOKEN_API)
+bot = Bot(token=TELEGRAM_TOKEN_API)
 dp = Dispatcher()
+access_err_msg = "Я могу отвечать только пользователям из белого списка"
 
 
 @dp.message(CommandStart())
 async def cmd_start(message: types.Message) -> None:
-    kb = [
-        [types.KeyboardButton(text="1")],
-        [types.KeyboardButton(text="2")],
-        [types.KeyboardButton(text="3")],
-    ]
-    keyboard = types.ReplyKeyboardMarkup(
-        keyboard=kb,
-        resize_keyboard=True,
-        input_field_placeholder="Выберите номер"
-    )
-    await message.answer("Выберите номер", reply_markup=keyboard)
+    if message.from_user.id in USER_WHITELIST:
+        kb = [
+            [types.KeyboardButton(text="1")],
+            [types.KeyboardButton(text="2")],
+            [types.KeyboardButton(text="3")],
+        ]
+        keyboard = types.ReplyKeyboardMarkup(
+            keyboard=kb,
+            resize_keyboard=True,
+            input_field_placeholder="Выберите номер"
+        )
+        await message.answer("Выберите номер", reply_markup=keyboard)
+    else:
+        await message.answer(access_err_msg)
 
 
 @dp.message(F.text.lower() == "1")
 async def handle_number_1(message: types.Message):
-    await message.answer("Выбран номер 1")
+    if message.from_user.id in USER_WHITELIST:
+        await message.answer("Выбран номер 1")
+    else:
+        await message.answer(access_err_msg)
 
 
 @dp.message(F.text.lower() == "2")
 async def handle_number_2(message: types.Message):
-    await message.answer("Выбран номер 2")
+    if message.from_user.id in USER_WHITELIST:
+        await message.answer("Выбран номер 2")
+    else:
+        await message.answer(access_err_msg)
 
 
 @dp.message(F.text.lower() == "3")
 async def handle_number_3(message: types.Message):
-    await message.answer("Выбран номер 3")
+    if message.from_user.id in USER_WHITELIST:
+        await message.answer("Выбран номер 3")
+    else:
+        await message.answer(access_err_msg)
 
 
 async def main() -> None:
