e625e00de99fc4cf2659e5c38baf372f71e1b66d
agorelik <Aleksandr_Gorelik@center.rt.ru>
2024-03-12 23:50:38 +0300

    Initial commit

--- sms-watcher-telegram-bot/main.py
+++ sms-watcher-telegram-bot/main.py
@@ -1,6 +1,6 @@
 import asyncio
 import os
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import CommandStart
 
 
@@ -11,8 +11,33 @@ dp = Dispatcher()
 
 
 @dp.message(CommandStart())
-async def start_cmd(message: types.Message) -> None:
-    await message.answer("test")
+async def cmd_start(message: types.Message) -> None:
+    kb = [
+        [types.KeyboardButton(text="1")],
+        [types.KeyboardButton(text="2")],
+        [types.KeyboardButton(text="3")],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите номер"
+    )
+    await message.answer("Выберите номер", reply_markup=keyboard)
+
+
+@dp.message(F.text.lower() == "1")
+async def handle_number_1(message: types.Message):
+    await message.answer("Выбран номер 1")
+
+
+@dp.message(F.text.lower() == "2")
+async def handle_number_2(message: types.Message):
+    await message.answer("Выбран номер 2")
+
+
+@dp.message(F.text.lower() == "3")
+async def handle_number_3(message: types.Message):
+    await message.answer("Выбран номер 3")
 
 
 async def main() -> None:
